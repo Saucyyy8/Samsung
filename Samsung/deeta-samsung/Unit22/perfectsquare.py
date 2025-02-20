683428bc4653a01cefd1c7a66a3ffce1567abf22
Saucyyy8 <168626052+Saucyyy8@users.noreply.github.com>
2025-02-20 10:16:14 +0530

    Add files via upload

---
+++ Samsung/deeta-samsung/Unit22/perfectsquare.py
@@ -0,0 +1,7 @@
+import math
+num=int(input("Enter a positive number:"))
+root=math.sqrt(num)
+if root.is_integer():
+    print(f"{num} is perfect square")
+else:
+    print(f"{num} is not perfect sqaure")
