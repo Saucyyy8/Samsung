b25d29641889ecf4abf5b6958f64041f2a346743
Saucyyy8 <168626052+Saucyyy8@users.noreply.github.com>
2025-02-25 09:24:18 +0530

    Delete Samsung/deeta-samsung directory

--- Samsung/deeta-samsung/Unit22/perfectsquare.py
+++
@@ -1,7 +0,0 @@
-import math
-num=int(input("Enter a positive number:"))
-root=math.sqrt(num)
-if root.is_integer():
-    print(f"{num} is perfect square")
-else:
-    print(f"{num} is not perfect sqaure")
